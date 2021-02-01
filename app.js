f146a77b2f60399b5a205fec3d9ff99ba1d6e63a
Denys <sagottw@gmail.com>
2021-02-01 23:28:41 +0200

    finished site

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+
+// Swiper
+
+var swiper = new Swiper('.swiper-container', {
+    cssMode: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
 // LANGUAGE
 let languageSelect = document.querySelector(".language");
 let languageImg = document.querySelectorAll(".language-img");
